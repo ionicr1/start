84f7affdcaf5e1e89502d4ac190b8e21b2542cab
ionicr1 <ionmartone@gmail.com>
2021-08-20 17:14:46 -0400

    added a function

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -2,8 +2,18 @@
 
 using namespace std;
 
+void sum() {
+	int num1, num2;
+	cout<<"\nPlease type a number: ";
+	cin>>num1;
+	cout<<"\n\nPlease type another number: ";
+	cin>>num2;
+	cout<<"\n\nYour numbers summed: "<<num1+num2<<endl;
+}
+
 int main() {
 	cout<<"\nHello World!\n";
 	cout<<"The coronavirus is getting worse, more insidious.\n\n";
+	sum();
 	return 0;
 }
